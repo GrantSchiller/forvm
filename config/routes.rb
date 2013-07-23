b87ff47d7167d9725e6fa809ac916ddf1cf46aaf
Max Luzuriaga <max@luzuriaga.com>
2013-07-23 11:01:38 -0400

    Removed test Heroku view; it works.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
-Forvm::Application.routes.draw do
-  get "/" => "application#index"
-  
+Forvm::Application.routes.draw do  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
