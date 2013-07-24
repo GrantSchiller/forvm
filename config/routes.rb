e91c25df415c99ef87c5f9f636a20d5347d8c929
Max Luzuriaga <max@luzuriaga.com>
2013-07-24 09:44:47 -0400

    Annotated routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -65,8 +65,19 @@ Forvm::Application.routes.draw do
   #   end
 end
 #== Route Map
-# Generated on 23 Jul 2013 19:22
+# Generated on 24 Jul 2013 09:44
 #
-#  login GET      /login(.:format)                   sessions#new
-# logout GET      /logout(.:format)                  sessions#destroy
-#        GET|POST /auth/:provider/callback(.:format) sessions#create
+#      root GET      /                                  pages#home
+# edit_user GET      /users/:id/edit(.:format)          users#edit
+#      user GET      /users/:id(.:format)               users#show
+#           PATCH    /users/:id(.:format)               users#update
+#           PUT      /users/:id(.:format)               users#update
+#           DELETE   /users/:id(.:format)               users#destroy
+#   schools GET      /schools(.:format)                 schools#index
+#    school GET      /schools/:id(.:format)             schools#show
+#    cities GET      /cities(.:format)                  cities#index
+#      city GET      /cities/:id(.:format)              cities#show
+#     login GET      /login(.:format)                   sessions#new
+#    logout GET      /logout(.:format)                  sessions#destroy
+#           GET|POST /auth/:provider/callback(.:format) sessions#create
+#    search GET      /search(.:format)                  search#index
