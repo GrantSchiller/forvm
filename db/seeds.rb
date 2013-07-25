d7c3629aa26e34d10459de49736d410f0dfec499
Max Luzuriaga <max@luzuriaga.com>
2013-07-25 00:23:57 -0400

    DB seeding with school list file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+file = File.new("db/schools.txt", "r")
+while (name = file.gets)
+  array = name.chomp.split(" : ")
+  
+  unless name.empty?
+    s = School.create(name: array[0])
+    
+    city = City.find_or_create_by(name: array[1])
+      
+    s.city = city
+    s.save
+  end
+end
